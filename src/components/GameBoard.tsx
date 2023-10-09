a56225cad92dc34a3c52585c29a3875b10627ed3
Clint G <cgardner4@gmail.com>
2023-10-09 18:31:11 +0200

    add tests for gameboard class to place ship in valid spot

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -2,8 +2,8 @@ import Ship from "../components/Ship";
 import { ShipProps } from "../components/Ship";
 
 interface GameBoardProps {
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
 }
 
 interface AttackResult {
@@ -18,46 +18,53 @@ export default class Gameboard {
   missedAttacks: number[][];
 
   constructor(props: GameBoardProps) {
-    this.width = props.width;
-    this.height = props.height;
+    this.width = props.width ?? 10;
+    this.height = props.height ?? 10;
     this.ships = [];
     this.missedAttacks = [];
   }
 
-  placeShip(newShip: Ship, x: number, y: number, isVertical: boolean) {
+  getShipSegment(newShip: Ship, x: number, y: number, isVertical: boolean) {
     const positions: [number, number][] = [];
 
     for (let i = 0; i < newShip.length; i++) {
       const position: [number, number] = isVertical ? [x, y + i] : [x + i, y];
       positions.push(position);
     }
-
-    const outOfBounds = positions.some(
+    return positions
+  }
+// returns true if the ship was placed out of bounds
+  isOutOfBounds(
+    positions: [number, number][],
+  ): boolean {
+    return positions.some(
       ([x, y]) => x < 0 || x >= this.width || y < 0 || y >= this.height
     );
+  }
 
-    if (outOfBounds) {
-      return false;
-    }
-
-    const doesNewShipOverlap = (): boolean => {
-      for (const existingShip of this.ships) {
-        for (const [x, y] of newShip.location) {
-          console.log(`Checking segment (${x}, ${y}) of new ship`);
-          if (existingShip.location.some(([ex, ey]) => ex === x && ey === y)) {
-            console.log(`Checking segment (${ex}, ${ey}) of existing ship`);
-            return true;
-          }
+  // returns true if the ship overlaps with another ship
+  doesNewShipOverlap(positions: [number, number][], ships: Ship[]): boolean {
+    for (const existingShip of ships) {
+      for (const [x, y] of positions) {
+        console.log(`Checking segment (${x}, ${y}) of new ship`);
+        if (existingShip.location.some(([ex, ey]) => ex === x && ey === y)) {
+          return true;
         }
       }
-      return false;
-    };
-
-    if (doesNewShipOverlap()) {
-      return false;
     }
+    return false;
+  }
+
+  // Finalize placement of the ship if valid
+
+  finalizeShipPlacement(newShip: Ship, positions: [number, number][]) {
     newShip.setLocation(positions);
     this.ships.push(newShip);
-    return true;
   }
+  // if (doesNewShipOverlap(newShip, this.ships)) {
+  //   return false;
+  // }
+  // newShip.setLocation(positions);
+  // this.ships.push(newShip);
+  // return true;
 }
--- src/tests/Gameboard.test.tsx
+++ src/tests/Gameboard.test.tsx
@@ -2,56 +2,44 @@ import Gameboard from "../components/GameBoard";
 import Ship from "../components/Ship";
 
 describe("Gameboard", () => {
-  describe("placeShip", () => {
-    it("should return true if ship can be placed on empty board", () => {
+  describe("getShipSegment", () => {
+    it("should return segment of placed ship", () => {
       const gameboard = new Gameboard({ width: 10, height: 10 });
-      const shipProps = { length: 3 };
+      const shipProps = { length: 4 };
       const newShip = new Ship(shipProps);
-      const result = gameboard.placeShip(newShip, 0, 0, true);
-      expect(result).toBe(true);
+      const result = gameboard.getShipSegment(newShip, 0, 0, false);
+      expect(result).toStrictEqual([
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+      ]);
     });
 
-    it("should return false if ship is out of bounds", () => {
+    it("should return true if ship is out of bounds", () => {
       const gameboard = new Gameboard({ width: 10, height: 10 });
       const shipProps = { length: 3 };
       const newShip = new Ship(shipProps);
-      const result = gameboard.placeShip(newShip, 7, 7, true);
+      const positions = gameboard.getShipSegment(newShip, 9, 9, true);
+      const result = gameboard.isOutOfBounds(positions);
       expect(result).toBe(true);
     });
 
     it("Show ship location", () => {
       const gameboard = new Gameboard({ width: 10, height: 10 });
-      const shipProps = { length: 3 };
+      const shipProps = { length: 4 };
       const newShip = new Ship(shipProps);
-      gameboard.placeShip(newShip, 1, 1, true);
-      expect(newShip.location).toEqual([
+      const result = gameboard.getShipSegment(newShip, 1, 1, true);
+      expect(result).toEqual([
         [1, 1],
         [1, 2],
         [1, 3],
+        [1, 4],
       ]);
     });
 
-    it("should return false if ship overlaps with existing ship", () => {
-      const gameboard = new Gameboard({ width: 10, height: 10 });
-      const shipProps1 = { length: 3 };
-      const shipProps2 = { length: 2 };
-      const newShip1 = new Ship(shipProps1);
-      const newShip2 = new Ship(shipProps2);
-      gameboard.placeShip(newShip1, 0, 0, true);
-      const result = gameboard.placeShip(newShip2, 0, 0, false);
-      expect(newShip1.location).toEqual([
-        [0, 0],
-        [0, 1],
-        [0, 2],
-      ]);
-      expect(newShip2.location).toEqual([
-        [0, 0],
-        [1, 0],
-      ]);
-      expect(result).toBe(false);
-    });
 
-    it.only("doesNewShipOverlap detects overlap correctly", () => {
+    it("doesNewShipOverlap detects overlap correctly", () => {
       const gameboard = new Gameboard({ width: 10, height: 10 });
       const ship1 = new Ship({ length: 3 });
       const ship2 = new Ship({ length: 4 });
@@ -68,15 +56,18 @@ describe("Gameboard", () => {
         [2, 5],
       ]);
       ship3.setLocation([
-        [5, 5],
-        [5, 6],
+        [2, 2],
+        [2, 3],
       ]);
       gameboard.ships.push(ship1);
       gameboard.ships.push(ship2);
-      const overlap = gameboard.placeShip(ship3, 2, 5, false);
+      const overlap = gameboard.doesNewShipOverlap(
+        ship3.location,
+        gameboard.ships
+      );
       expect(overlap).toBe(true);
-      const noOverlap = gameboard.placeShip(ship3, 7, 7, false);
-      expect(noOverlap).toBe(true);
+      // const noOverlap = gameboard.placeShip(ship3, 7, 7, false);
+      // expect(noOverlap).toBe(true);
     });
 
     it("should add ship to ships array if ship can be placed", () => {
@@ -85,18 +76,34 @@ describe("Gameboard", () => {
       const shipProps2 = { length: 2 };
       const newShip1 = new Ship(shipProps1);
       const newShip2 = new Ship(shipProps2);
-      gameboard.placeShip(newShip1, 0, 0, true);
-      gameboard.placeShip(newShip2, 0, 0, true);
+      const ship1Segement = gameboard.getShipSegment(newShip1, 0, 0, true);
+      const ship2Segement = gameboard.getShipSegment(newShip2, 3, 3, false);
+      const isInBounds1 = gameboard.isOutOfBounds(ship1Segement);
+      const isInBounds2 = gameboard.isOutOfBounds(ship2Segement);
+      const doesOverlap1 = gameboard.doesNewShipOverlap(
+        ship1Segement,
+        gameboard.ships
+      );
+      const doesOverlap2 = gameboard.doesNewShipOverlap(
+        ship2Segement,
+        gameboard.ships
+      );
+      if (!isInBounds1 && !doesOverlap1) {
+        gameboard.finalizeShipPlacement(newShip1, ship1Segement);
+      }
+      if (!isInBounds2 && !doesOverlap2) {
+        gameboard.finalizeShipPlacement(newShip2, ship2Segement);
+      }
       expect(newShip1.location).toEqual([
         [0, 0],
         [0, 1],
         [0, 2],
       ]);
       expect(newShip2.location).toEqual([
-        [0, 0],
-        [0, 1],
+        [3, 3],
+        [4, 3],
       ]);
-      expect(gameboard.ships.length).toBe(1);
+      expect(gameboard.ships.length).toBe(2);
       expect(gameboard.ships[0]).toBeInstanceOf(Ship);
       expect(gameboard.ships[0].length).toBe(shipProps1.length);
     });
